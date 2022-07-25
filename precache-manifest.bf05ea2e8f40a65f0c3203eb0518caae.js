d16f2510715d8b0456cb9887aeadc2b82ae195f9
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-24 23:20:12 -0300

    Updates

---
+++ precache-manifest.bf05ea2e8f40a65f0c3203eb0518caae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571c7302727a80858bf58ff1f447d3f5",
+    "url": "/online-store-deploy/index.html"
+  },
+  {
+    "revision": "93028789f909f795e3b9",
+    "url": "/online-store-deploy/static/css/main.aaac9626.chunk.css"
+  },
+  {
+    "revision": "ddeb249ae5b6ec86c418",
+    "url": "/online-store-deploy/static/js/2.5a888fb3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store-deploy/static/js/2.5a888fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93028789f909f795e3b9",
+    "url": "/online-store-deploy/static/js/main.ed70156c.chunk.js"
+  },
+  {
+    "revision": "88eea87096aa3521e34d",
+    "url": "/online-store-deploy/static/js/runtime-main.44725229.js"
+  }
+]);
